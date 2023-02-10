aa11ca69341c733fbeddf97afc131732176e881c
Noam Lushi <noamlushi@gmail.com>
2023-02-10 20:28:14 +0200

    add desti extra

--- src/components/Admin/AddDesti.js
+++ src/components/Admin/AddDesti.js
@@ -68,6 +68,186 @@ function AddDesti() {
               className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>{" "}
+          <div className="mb-2">
+            <label
+              htmlFor="priceTicket"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              priceTicket
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="gallery-1"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              gallery-1
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="gallery-2"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              gallery-2
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="gallery-3"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              gallery-3
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="gallery-4"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              gallery-4
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-1-name
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-url"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-1-url
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-2-name
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-url"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-2-url
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-3-name
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-url"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-3-url
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-4-name
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-url"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-4-url
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-name"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-5-name
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="attraction-1-url"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              attraction-5-url
+            </label>
+            <input
+              type="text"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            />
+          </div>
           <div className="mt-6">
             <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
               submit
